4966ab93c2e7ba86d22c601f56d750beec099058
Pedro Hincapie <pedandniv@gmail.com>
2017-11-09 22:24:36 -0500

    Codility ejercicio numero no contenido en array

--- NumNoContenidoArray.java
+++ NumNoContenidoArray.java
@@ -1,43 +1,50 @@
 package co.com.ejercccios.logica;
 
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class NumNoContenidoArray {
 
 	public static void main(String[] args) {
 
 		//		int A[] = {1,2,3,6,7,8,56,9,86};
-//		int A[] = {1, 3, 6, 4, 1, 2};
-						int A[] = {1,2,3};
-//						int A[] = {-1,-3};
+		int A[] = {1, 3, 6, 4, 1, 2};
+		//		int A[] = {1,2,3};
+		//				int A[] = {-1,-3};
 
-		HashMap<Integer, Integer> contenedor = new HashMap<>();
-		int dato = 0;
+		Map<Integer, Integer> contenedor = new LinkedHashMap<Integer, Integer>();
 
 		int cantidad = A.length;
 
 
 		for (int i = 0; i < cantidad; i++) {
-			contenedor.put(A[i],A[i]);
+			contenedor.put(i+1, i+1);
 		}
 
 		//		for(Map.Entry m:contenedor.entrySet()){
-		//			System.err.println("Dato " + m.getValue());
+		//			System.err.println("Dato " + m.getKey());
 		//		}
 
 		for (int i = 0; i < cantidad ; i++) {
-			if(!contenedor.containsValue(i)){
-				dato = i;
-			}else{
-				dato = contenedor.get(i) + 1;
+			if(contenedor.containsKey(A[i])){
+				contenedor.remove(A[i]); 
 			}
 
 		}
-		
-		if(contenedor.containsValue(dato)){
-			dato++;
+
+		if(contenedor.size() == 0){
+			System.err.println(cantidad+1);
+
+		}else{
+			Iterator<Integer> numeroContenidos = contenedor.keySet().iterator();
+
+			if(numeroContenidos.hasNext()){
+
+				System.err.println((int)numeroContenidos.next());
+			}
 		}
 
-		System.err.println(dato);
+		System.err.println(1);
 	}
 }
